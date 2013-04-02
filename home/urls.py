e98777ff1db58dff5a505cafe19c47d7b921f88f
Pablo Ricco <pricco@gmail.com>
2013-04-02 19:32:06 -0300

    urls for rsvp, map and card

--- home/urls.py
+++ home/urls.py
@@ -6,7 +6,9 @@ from home.views import ConfirmView
 urlpatterns = patterns('',
     (r'^$', IndexView.as_view()),
     (r'^(?P<code>[a-zA-Z0-9]{3})/?$', IndexView.as_view()),
-    (r'^(?P<code>[a-zA-Z0-9]{3})/card/?$', IndexView.as_view(card=True)),
+    (r'^(?P<code>[a-zA-Z0-9]{3})/card/?$', IndexView.as_view(mode='card')),
+    (r'^(?P<code>[a-zA-Z0-9]{3})/rsvp/?$', IndexView.as_view(mode='rsvp')),
+    (r'^(?P<code>[a-zA-Z0-9]{3})/map/?$', IndexView.as_view(mode='map')),
     (r'^code/?$', CodeView.as_view()),
     (r'^confirm/?$', ConfirmView.as_view()),
 )
--- home/views/index.py
+++ home/views/index.py
@@ -8,7 +8,7 @@ from django.http import HttpResponseRedirect
 class IndexView(TemplateView, GroupMixin):
 
     template_name = 'index.html'
-    card = False
+    mode = None
 
     def get(self, request, code=None, *args, **kwargs):
         group = self.get_group(request, code)
@@ -17,7 +17,7 @@ class IndexView(TemplateView, GroupMixin):
         else:
             kwargs['songs'] = Song.objects.filter(active=True).order_by('order', 'id')
             kwargs['locations'] = Location.objects.filter(active=True)
-            kwargs['card'] = group and self.card
+            kwargs['mode'] = group and self.mode
             kwargs['group'] = group
             response = super(IndexView, self).get(request, *args, **kwargs)
         self.set_group(response, group)
